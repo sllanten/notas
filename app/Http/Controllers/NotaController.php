26c704094346748c86d89924f5e436f946b3c5e5
sllanten <dev.sllanten@gmail.com>
2024-11-29 18:11:54 -0500

    Implementacion de controller y route de Api

--- app/Http/Controllers/NotaController.php
+++ app/Http/Controllers/NotaController.php
@@ -44,4 +44,29 @@ class NotaController extends Controller
         return redirect()->route('wellcome');
 
     }
+
+    public function apiGet(){
+        return response()->json(json_decode($this->getData()));
+    }
+
+    public function apiStorage(Request $request){
+        $request->validate([
+            'status' => 'required|string|max:4',
+            ],[
+               'status.required' => 'El campo status es obligatorio.',
+               'status.max' => 'El campo status como maximo se espera 4 caracteres.',
+            ]);
+
+        if(($_POST['status']!= "congration") || ($_POST['status']!= "fail") || ($_POST['status']!= "test")){
+            return response()->json(['error' => 'Invalid status'], 400);
+        }
+
+        $ntNew= new Nota();
+        $ntNew->status= $request->status;
+        $ntNew->save();
+
+        return response()->json(['status' => 'success'], 200);
+    }
+
+
 }
